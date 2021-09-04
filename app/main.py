7a7b253d52de7d4f3df348979757959b19cbce2e
baroulescientifique <diabyoumar22@yahoo.fr>
2021-09-04 13:03:29 +0300

    The new method for items has been added successfully

--- app/main.py
+++ app/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get('/get-started')
 def getStarted():
     return {'Response': 'Hello guys and welcome...'}
+    
+    
+    
+@app.get('/items/{item_id}')
+def getItem(item_id):
+    return {'Response': f'The item you selected costs 20 Euros'}
